8ccc9a01a01076c7a8184e89175ea67f7cb00f98
marzi ghased <marzighased@gmail.com>
2025-01-12 12:26:06 +0100

    second commit

---
+++ src/app/imprint/imprint.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-imprint',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './imprint.component.html',
+  styleUrl: './imprint.component.scss'
+})
+export class ImprintComponent {
+
+}
